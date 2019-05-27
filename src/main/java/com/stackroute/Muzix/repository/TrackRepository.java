c0166260873a2210ade05bf9ddfd9dca66b256d7
DipaliRShinde <drs1454006@gmail.com>
2019-05-27 12:36:47 +0530

    Initial Push

---
+++ src/main/java/com/stackroute/Muzix/repository/TrackRepository.java
@@ -0,0 +1,15 @@
+package com.stackroute.Muzix.repository;
+
+import com.stackroute.Muzix.domain.Track;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TrackRepository extends CrudRepository<Track, Integer> {
+
+    @Query(value = "select * from track where track_name=?1", nativeQuery = true)
+    public List<Track> getTrackByName(String trackName);
+}
